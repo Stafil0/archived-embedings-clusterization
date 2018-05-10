9edad2af2b02807804617efb02204aaf2d60b9d4
Stafil0 <stafilo13@gmail.com>
2018-05-10 19:58:02 +0400

    конвертер нормально обрабатывает множественные пробелы и символы по краям

--- src/PdfConverter/Converter.cs
+++ src/PdfConverter/Converter.cs
@@ -10,8 +10,9 @@ namespace MTA.PdfConverter
   public static class Converter
   {
     private static readonly string[] Inlines = { "лат", "греч", "др", "англ", "см", "нм", "тыс", "ат" };
+    private static readonly char[] TrimSymbols = { ',', '.', ' ', '?', '!', '-', 'Ч' };
     private static readonly string MatchesRegex = $@"((?:(?(?=(\b{string.Join(@"\.|\b", Inlines)}\.|[^а-€ј-яa-zA-Z][а-€ј-яa-zA-Z]\.\s?|\d+\.\d))\2|[^.!?])+[.!?]))";
-    private const string PunctuationRegex = @"[^а-€ј-яa-zA-Z]+";
+    private const string PunctuationRegex = @"[^а-€ј-яa-zA-Z]{2,}";
     private const string ShortWordsRegex = @"\b[а-€ј-яa-zA-Z]{1,2}\b";
 
     public static string GetTextFromFile(string path)
@@ -56,7 +57,7 @@ namespace MTA.PdfConverter
           var formatted = sentence.Replace("-\r\n", string.Empty);
           formatted = Regex.Replace(formatted, PunctuationRegex, separator, RegexOptions.IgnoreCase);
           formatted = Regex.Replace(formatted, ShortWordsRegex, string.Empty, RegexOptions.IgnoreCase);
-          formatted = formatted.ToLower().Trim();
+          formatted = formatted.ToLower().Trim(TrimSymbols);
 
           if (formatted.IsSentence())
             yield return formatted;
